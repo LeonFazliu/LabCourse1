1cc86968dfe57441e52f67fbd65a8924b43331a0
TukuLee <121036323+TukuLeejr@users.noreply.github.com>
2024-04-12 13:12:56 +0200

    shtimi i opsionit per shtimin,editmin dhe fshirjen e Hoteleve

---
+++ frontend/src/Cruds/HotelCrud/AddEditHotel.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link, useParams } from "react-router-dom"; // Correct import
+import axios from 'axios';
+import { toast } from "react-toastify";
+import "./AddEdit.css"
+
+const initialState = {
+  hotelname: "",
+  hotelcity: ""
+};
+
+const AddEditHotel = () => {
+  const [state, setState] = useState(initialState);
+  const { hotelname, hotelcity } = state;
+  const navigate = useNavigate(); 
+  const { id } = useParams();
+  
+  useEffect(() => {
+    if (id) {
+      axios.get(`http://localhost:5000/api/hotels/${id}`)
+        .then(resp => setState({ ...resp.data[0] }))
+        .catch(error => console.error(error));
+    }
+  }, [id]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!hotelname || !hotelcity) {
+      toast.error("Please fill each input field");
+    } else {
+      if (!id) {
+        axios.post("http://localhost:5000/api/hotels", {
+          hotelname,
+          hotelcity
+        })
+          .then(() => {
+            setState(initialState);
+            toast.success("Hotel Added Successfully");
+          })
+          .catch((err) => toast.error(err.response.data));
+      } else {
+        axios.put(`http://localhost:5000/api/hotels/${id}`, {
+          hotelname,
+          hotelcity
+        })
+          .then(() => {
+            toast.success("Hotel Updated Successfully");
+          })
+          .catch((err) => toast.error(err.response.data));
+      }
+      navigate("/hotels");
+    }
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center"
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor='hotelname'>Hotel Name</label>
+        <input
+          type='text'
+          id='hotelname'
+          name='hotelname'
+          placeholder='Hotel Name....'
+          value={hotelname || ""}
+          onChange={handleInputChange}
+        />
+        <label htmlFor='hotelcity'>City</label>
+        <input
+          type='text'
+          id='hotelcity'
+          name='hotelcity'
+          placeholder='City....'
+          value={hotelcity || ""}
+          onChange={handleInputChange}
+        />
+        <input type='submit' value={id ? "Update" : "Save"} />
+        <Link to="/hotels">
+          <input type="button" value="Go Back"/>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default AddEditHotel;
---
+++ frontend/src/Cruds/HotelCrud/ViewHotel.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from "react-router-dom";
+import axios from 'axios';
+import "./View.css";
+
+export const ViewHotel = () => {
+    const [hotel, setHotel] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/hotels/${id}`)
+            .then(resp => setHotel({ ...resp.data[0] }))
+            .catch(error => console.error(error));
+    }, [id]);
+
+    return (
+        <div style={{ marginTop: "150px" }}>
+            <div className='card'>
+                <div className='card-header'>
+                    <p>Hotel Details</p>
+                </div>
+                <div className='container'>
+                    <strong>ID:</strong>
+                    <span>{id}</span>
+                    <br />
+                    <br />
+                    <strong>Hotel Name:</strong>
+                    <span>{hotel.hotelname}</span>
+                    <br />
+                    <br />
+                    <strong>City:</strong>
+                    <span>{hotel.hotelcity}</span>
+                    <br />
+                    <br />
+                    <Link to="/hotels">
+                        <div className='btn btn-edit'>
+                            Go Back
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ViewHotel;
